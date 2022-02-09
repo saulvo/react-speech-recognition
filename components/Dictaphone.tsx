8a4df27167002feedbceeeb28b05f30ca23f7a54
GK-SONIT <vtson23597@gmail.com>
2022-02-09 11:29:54 +0700

    init

---
+++ components/Dictaphone.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const Dictaphone = () => {
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>{`Browser doesn't support speech recognition.`}</span>;
+  }
+
+  const startListening = () => {
+    SpeechRecognition.startListening({ language: 'vn-VN' });
+  };
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
